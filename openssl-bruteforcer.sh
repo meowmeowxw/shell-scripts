bc3c98bf12c120a3d2a612a53574a4e12743baf4
meowmeowxw <giovanni.disanti1@gmail.com>
2018-09-28 15:58:55 +0200

    openssl wordlist bruteforcer

---
+++ openssl-bruteforcer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#The first argument to pass to the function is the wordlist to use
+#The second argument is the File to decrypt
+#I consider the file decrypted when I can read the ASCII code
+#If the file to decrypt that you want to decrypt is not a text file then you need to remove the 13-19 lines.
+
+WORDLIST=$1 
+KEYFILE=$2
+for PASSWORD in $( cat $WORDLIST ); 
+do
+	openssl enc -aes-256-cbc -salt -d -in $KEYFILE -out decrypt_file.txt -pass pass:$PASSWORD 2> /dev/null
+	if [ $? -eq 0 ]
+	then
+		file decrypt_file.txt | grep ASCII >/dev/null 
+			if [ $? -eq 0 ]
+			then 	
+				echo "The password is: " $PASSWORD	
+				echo "output saved on: decrypt_file.txt"
+				break
+			fi
+	fi
+done
+
